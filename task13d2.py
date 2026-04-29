d8858600d46b658dcd35a52d87ee37a0f401548b
amruthalakshmid2007-bit <amruthalakshmid2007@gmail.com>
2026-04-29 12:11:53 +0530

    Update task13d2.py

--- task13d2.py
+++ task13d2.py
@@ -1,4 +1,8 @@
-num=int(input("enter value"))
-i=0
-while i<=num:
-  print(f"{num}\n")
+#reversal of input without inbuilt commands
+num=int(input("enter"))#12345
+total=0 #54321
+while num>0: #0>0#False
+  last=num%10 #1
+  total=total*10+last
+  num=num//10
+print(total)
