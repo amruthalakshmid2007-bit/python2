9fc300f4b811663df7268fb4d4a9f610b8a26bb3
amruthalakshmid2007-bit <amruthalakshmid2007@gmail.com>
2026-04-29 12:18:09 +0530

    Update task13d2.py

--- task13d2.py
+++ task13d2.py
@@ -1,8 +1,15 @@
 #reversal of input without inbuilt commands
 num=int(input("enter"))#12345
+original=num
 total=0 #54321
 while num>0: #0>0#False
   last=num%10 #1
   total=total*10+last
   num=num//10
-print(total)
+print(total)  
+#to check whether it is palindrome or not
+if original==total:
+  print("it is palindrome")
+else:
+   print("it is not palindrome")
+ 
