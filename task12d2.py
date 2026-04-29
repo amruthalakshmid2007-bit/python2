3cba93be67615c5d787e82ef7d7c3c57fea3d57b
amruthalakshmid2007-bit <amruthalakshmid2007@gmail.com>
2026-04-29 12:01:06 +0530

    Update task12d2.py

---
+++ task12d2.py
@@ -0,0 +1,15 @@
+#12345#imp for interviews
+num=int(input("enter:"))
+total=0 #0
+while num>0:#12345>0 #true
+  last=num%10 #5
+  total+=last
+  num=num//10 #1234(flow division
+  #print(total) (here to know the steps)
+print(total)  #(direct answer)
+# output for 12345
+# 5
+# 9
+# 12
+# 14
+# 15
